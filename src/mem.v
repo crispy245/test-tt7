1e66f411325547bcdb3d0cec790188c2463a570c
Aldo <aldobalsamoreyes@gmail.com>
2024-05-31 20:16:33 +0800

    eliminating memory

--- src/mem.v
+++ src/mem.v
@@ -25,39 +25,50 @@ module mem
   input  wire                 wren,
   output reg  [WIDTH-1:0]     q
 );
+  // //verilator lint_off UNUSEDSIGNAL
 
-  reg [WIDTH-1:0] mem [0:DEPTH-1] /* synthesis ramstyle = "M20K" */;
+  wire clk = clock;
+  wire data_i = data;
+  wire ene = rden;
+  wire wene = wren;
+  assign q = data;
+  wire [`CLOG2(DEPTH)-1:0] rd = rdaddress;
+  wire [`CLOG2(DEPTH)-1:0] wr = wraddress;
+  // //verilator lint_off UNUSEDSIGNAL
 
-  //verilator lint_off UNUSEDSIGNAL
-  integer file;
-  integer scan;
-  //verilator lint_off UNUSEDSIGNAL
-  integer i;
 
-  initial
-    begin
-      // read file contents if FILE is given
-      if (FILE != "")
-        // If you get error here, check path to FILE at bottom of generated butterfly.v
-        $readmemb(FILE, mem);
+  // reg [WIDTH-1:0] mem [0:DEPTH-1] /* synthesis ramstyle = "M20K" */;
 
-      // set all data to 0 if INIT is true
-      if (INIT)
-        for (i = 0; i < DEPTH; i = i + 1)
-          mem[i] = {WIDTH{1'b0}};
-   end
+  // //verilator lint_off UNUSEDSIGNAL
+  // integer file;
+  // integer scan;
+  // //verilator lint_off UNUSEDSIGNAL
+  // integer i;
 
-  always @ (posedge clock)
-  begin
-    if (wren)
-      mem[wraddress] <= data;
-  end
+  // initial
+  //   begin
+  //     // read file contents if FILE is given
+  //     if (FILE != "")
+  //       // If you get error here, check path to FILE at bottom of generated butterfly.v
+  //       $readmemb(FILE, mem);
 
-  always @ (posedge clock)
-  begin
-    if (rden)
-      q <= mem[rdaddress];
-  end
+  //     // set all data to 0 if INIT is true
+  //     if (INIT)
+  //       for (i = 0; i < DEPTH; i = i + 1)
+  //         mem[i] = {WIDTH{1'b0}};
+  //  end
+
+  // always @ (posedge clock)
+  // begin
+  //   if (wren)
+  //     mem[wraddress] <= data;
+  // end
+
+  // always @ (posedge clock)
+  // begin
+  //   if (rden)
+  //     q <= mem[rdaddress];
+  // end
 
 endmodule
 
