44b431934b458bbfcbe257c8e4bf6065104b8a18
Aldo <aldobalsamoreyes@gmail.com>
2024-05-31 14:14:19 +0800

    please god

--- src/project.v
+++ src/project.v
@@ -18,6 +18,7 @@ module tt_um_example (
 
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_oe  = 0;
+  assign uio_out[7:3] = ui_in[7:4] + ui_in[2] + uio_in[7] + uio_in[5:0];
 
   
 
